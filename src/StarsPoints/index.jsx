3978988a76afe4c14cbeef737ac9c8d9564fd835
Obed Paz <lesterpaz99@gmail.com>
2022-02-08 16:19:32 -0600

    create StarPoints component

---
+++ src/StarsPoints/index.jsx
@@ -0,0 +1,17 @@
+import './StarsPoints.css';
+
+const StarsPoints = () => {
+	return (
+		<div className='stars-wrapper'>
+			<section className='icon-list'>
+				<i className='nes-icon is-medium star'></i>
+				<i className='nes-icon is-medium star'></i>
+				<i className='nes-icon is-medium star'></i>
+				<i className='nes-icon is-medium star'></i>
+				<i className='nes-icon is-medium star'></i>
+			</section>
+		</div>
+	);
+};
+
+export { StarsPoints };
