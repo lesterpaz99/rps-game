931e928488bbacb27f108bba3f8bc036cfc8b1fd
Obed Paz <lesterpaz99@gmail.com>
2022-02-08 14:16:47 -0600

    wrap the app with global context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import { GlobalContextProvider } from './context/globalContext';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+	<GlobalContextProvider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</GlobalContextProvider>,
+	document.getElementById('root')
+);
